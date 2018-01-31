68db9d012dd91d0e348473267031a38fb43ba49a
Gavin Morris <gavin1997m@gmail.com>
2018-01-31 19:45:16 +0000

    Commented and Disabled resizability on JFrame

--- CluedoGame/src/Board.java
+++ CluedoGame/src/Board.java
@@ -1,14 +1,32 @@
 import javax.swing.*;
+
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+
+/*
+	Builds the board image into a JPanel
+ */
+
 
 public class Board extends Main {
 	
+	//import image as an ImageIcon
 	ImageIcon image = new ImageIcon("cluedoboard.jpg");
+	
+	//Put image into a JLabel
     JLabel label = new JLabel(image);
+    
+    //Create a JPanel
 	JPanel panel = new JPanel();
 
+	
+	//Constructor for the display panel
 	public Board() {
+		
+		//Layout of JPanel
 		panel.setLayout(new GridBagLayout());
+		
+		//add JLabel to the JPanel
         panel.add(label);	
 	}
 		
--- CluedoGame/src/SendMessageButtonListener.java
+++ CluedoGame/src/SendMessageButtonListener.java
@@ -1,19 +1,27 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/*
+	ActionListener to echo text onto display JTextArea
+ */
+
 class SendMessageButtonListener implements ActionListener {
-		
-	String  username = "jembaba58";
 	
+	//SMH!
+	String  userTag = "jembaba58";
+	
+	//connects to display class and textualCommand class
 	public void actionPerformed(ActionEvent event) {
 		buttonfunction(Main.display, Main.textualcommand);
 	}
 	
+	
+	//Takes from textualCommand gives to display
 	public void buttonfunction(Display display, TextualCommand textualcommand) {
 		if (textualcommand.textfield.getText().length() < 1) {
 			// do nothing
 		} else {
-			display.textarea.append("<"  + username +  ">: " +  textualcommand.textfield.getText() + "\n");
+			display.textarea.append("<" + userTag+">: " +  textualcommand.textfield.getText() + "\n");
 			textualcommand.textfield.setText("");
 		}
 	}
