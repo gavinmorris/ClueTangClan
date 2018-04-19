7758cbbd07aed9f7c2e57ffdb8cb76d6e4b29434
Euan Hayden <euanhayden@gmail.com>
2018-04-19 16:19:07 +0100

    Update

--- CluedoBotAssignment5/src/bots/ClueTangClan.java
+++ CluedoBotAssignment5/src/bots/ClueTangClan.java
@@ -369,6 +369,36 @@ public class ClueTangClan implements BotAPI {
     	
     	return returnString;
     }
+
+	@Override
+	public String getVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void notifyPlayerName(String playerName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyTurnOver(String playerName, String position) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyQuery(String playerName, String query) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyReply(String playerName, boolean cardShown) {
+		// TODO Auto-generated method stub
+		
+	}
     
     
 
